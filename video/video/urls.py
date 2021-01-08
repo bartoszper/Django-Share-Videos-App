22b4cdf953a29def64f932060dd86c8387d00ccf
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-08 19:16:38 +0100

    Add dynamic url's

--- video/video/urls.py
+++ video/video/urls.py
@@ -24,9 +24,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     #Auth
-    path('signup', views.SignUp.as_view, name='signup'),
-    path('login', auth_views.LoginView.as_view(), name='login'),
-    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
