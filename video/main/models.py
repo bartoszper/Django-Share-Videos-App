2765153ac1f0245140dc908833c409b3313178c1
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-08 19:42:38 +0100

    Add models

--- video/main/models.py
+++ video/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    youtube_id = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
