497d7c7cc5f378a29223750d4264595f97e4775b
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-08 20:37:32 +0100

    Add class CreateCategory

--- video/main/views.py
+++ video/main/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from .models import Category
 
 
 # Create your views here.
@@ -12,3 +13,15 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class CreateCategory(generic.CreateView):
+    model = Category
+    fields = ['title']
+    template_name = 'main/create_category.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateCategory, self).form_valid(form)
+        return redirect('home')
+
